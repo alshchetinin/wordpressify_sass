87a9971f53dd0e485532b48e394a766aaa73a1b0
alshchetinin <alexsander.qw@gmail.com>
2020-03-21 20:34:11 +0300

    function fixed

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -7,46 +7,16 @@ function wordpressify_resources() {
 
 add_action( 'wp_enqueue_scripts', 'wordpressify_resources' );
 
-// Customize excerpt word count length
-function custom_excerpt_length() {
-	return 22;
+register_nav_menus(array(
+	'top'    => 'Верхнее меню'
+));
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title'    => __('Настройки сайта'),
+		'menu_title'    => __('Настройки сайта'),
+		'menu_slug'     => 'theme-general-settings'
+  ));
+	
 }
-
-add_filter( 'excerpt_length', 'custom_excerpt_length' );
-
-// Theme setup
-function wordpressify_setup() {
-	// Handle Titles
-	add_theme_support( 'title-tag' );
-
-	// Add featured image support
-	add_theme_support( 'post-thumbnails' );
-	add_image_size( 'small-thumbnail', 720, 720, true );
-	add_image_size( 'square-thumbnail', 80, 80, true );
-	add_image_size( 'banner-image', 1024, 1024, true );
-}
-
-add_action( 'after_setup_theme', 'wordpressify_setup' );
-
-show_admin_bar( false );
-
-// Checks if there are any posts in the results
-function is_search_has_results() {
-	return 0 != $GLOBALS['wp_query']->found_posts;
-}
-
-// Add Widget Areas
-function wordpressify_widgets() {
-	register_sidebar(
-		array(
-			'name'          => 'Sidebar',
-			'id'            => 'sidebar1',
-			'before_widget' => '<div class="widget-item">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-
-add_action( 'widgets_init', 'wordpressify_widgets' );
